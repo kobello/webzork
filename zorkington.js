9ce52869872d9c7d438e222fcc5b3dda1c115708
djmorosini <djmorosini@outlook.com>
2018-06-13 14:32:44 -0400

    taking sign

--- zorkington.js
+++ zorkington.js
@@ -10,8 +10,14 @@ process.stdin.on('data', (chunk) => {
     let action = chunk.toString().trim();
     if (action == "read sign") {
         console.log('The sign says "Welcome to Burlington Code Academy! Come on up to the second floor. If the door is locked, use the code 12345.')
+    } else if (action == "take sign") {
+        console.log("That would be selfish. How will other students find their way?")
+    } else {
+        console.log("Sorry, I don't know how to " + action)
     }
 });
+
+
 
 // function moveToRoom(newRoom) {
 //     if (canMoveToRoom(newRoom)) {
