d4fc41ac603940ba44aff21f6a2c57cb25c2bcf1
djmorosini <djmorosini@outlook.com>
2018-06-15 12:22:48 -0400

    removed duplicates and comments

--- zorkington.js
+++ zorkington.js
@@ -21,12 +21,8 @@ let currentRoom = "182 Main st."
 let key = "12345"
 let doorLocked = true
 let playerInventory = []
-// let oneEightTwoMain = []
-// let foyerInventory = ['Seven Days']
 
 console.log(currentRoom + "\n" + rooms[currentRoom]["description"])
-
-//console.log(currentRoom + "\nYou are standing on Main Street between Church and South Winooski. There is a door here. A keypad sits on the handle. On the door is a handwritten sign.")
 
 process.stdin.on('data', (chunk) => {
     let action = chunk.toString().trim();
@@ -47,31 +43,6 @@ process.stdin.on('data', (chunk) => {
     console.log(currentRoom)
 });
 
-function foyerActions(action) {
-    if (action == "drop paper" || action == "drop seven days") {
-        if (playerInventory != 0) {
-            rooms['Foyer']['inventory'].push(playerInventory.pop());
-            console.log("The copy of Seven Days is removed from the player's inventory");
-        }
-        else {
-            console.log("There's nothing in your inventory!");
-        }
-    }
-    else if (action == "take seven days" || action == 'take paper') {
-        if (rooms['Foyer']['inventory'].includes('Seven Days')) {
-            console.log("You pick up the paper and leaf through it looking for comics and ignoring the articles, just like everybody else does.");
-            take('Seven Days')
-            // playerInventory.push(rooms['Foyer']['inventory'].pop());
-        }
-        else {
-            console.log("You already picked that up!");
-        }
-    }
-    else {
-        console.log("Sorry, I don't know how to " + action + ".");
-    }
-}
-
 function changeRoom(newRoom) {
     let validTransitions = rooms[currentRoom].canChangeTo;
     if (validTransitions.includes(newRoom)) {
@@ -81,7 +52,6 @@ function changeRoom(newRoom) {
         console.log("Invalid state transition attempted - from " + currentRoom + " to " + newRoom);
     }
 }
-
 
 function take(itemFromAction) {
     if (rooms[currentRoom]["inventory"].includes(itemFromAction)) {
@@ -94,6 +64,7 @@ function take(itemFromAction) {
         console.log("" + rooms[currentRoom]["inventory"])
     }
 }
+
 function mainStActions(action) {
     if (action == "drop paper" || action == "drop seven days") {
         if (playerInventory != 0) {
@@ -114,9 +85,7 @@ function mainStActions(action) {
             console.log('The door is locked. There is a keypad on the handle.');
         }
     } else if (action.startsWith('key in') || action.startsWith('enter code')) {
-        // keyRe = /[1-9]+/
         if (key == action.match('12345')) {
-            // action == 'key in 12345' || action == 'enter code 12345')
             console.log('Success! The door opens. You enter the foyer and the door shuts behind you.');
             doorLocked = false;
             changeRoom("Foyer");
@@ -133,34 +102,17 @@ function foyerActions(action) {
         if (playerInventory != 0) {
             rooms['Foyer']['inventory'].push(playerInventory.pop());
             console.log("The copy of Seven Days is removed from the player's inventory");
-        }
-        else {
+        } else {
             console.log("There's nothing in your inventory!");
         }
     } else if (action == "take seven days" || action == 'take paper') {
         take('Seven Days')
-        
-        // if (rooms['Foyer']['inventory'].length != 0) {
-        //     console.log("You pick up the paper and leaf through it looking for comics and ignoring the articles, just like everybody else does.");
-        //     playerInventory.push(rooms['Foyer']['inventory'].pop());
-        //     console.log(playerInventory);
-        // }
-        // else {
-        //     console.log("You already picked that up!");
-        // }
     } else if (action == "go back") {
         changeRoom("182 Main st.")
     } else {
         console.log("Sorry, I don't know how to " + action + ".");
     }
 }
-
-// function moveToRoom(newRoom) {
-//     // if (canMoveToRoom(newRoom)) {
-//     currentRoom = newRoom;
-//     console.log('\nCurrent room: ' + currentRoom + "\n" + rooms[currentRoom]['description'])
-//     // }
-// }
 
 function inventory() {
     if (playerInventory.length <= 0) {
@@ -173,17 +125,17 @@ function inventory() {
     }
 }
 
-function take(desiredItem) {
-    const roomInventory = rooms[currentRoom]['inventory'];
+// function take(desiredItem) {
+//     const roomInventory = rooms[currentRoom]['inventory'];
 
-    if (roomInventory.includes(desiredItem)) {
+//     if (roomInventory.includes(desiredItem)) {
 
-        const itemFromRoom = roomInventory.splice(roomInventory.indexOf(desiredItem), 1).toString();
-    playerInventory.push(itemFromRoom)
-    console.log('' + desiredItem + ' was added to your inventory.')
-    } else {
-        console.log('There is no ' + desiredItem + ' here.')
-    }
+//         const itemFromRoom = roomInventory.splice(roomInventory.indexOf(desiredItem), 1).toString();
+//     playerInventory.push(itemFromRoom)
+//     console.log('' + desiredItem + ' was added to your inventory.')
+//     } else {
+//         console.log('There is no ' + desiredItem + ' here.')
+//     }
     // while (rooms[currentRoom]['inventory'].length > rooms[currentRoom]['inventory'].indexOf(itemFromAction)){
     //     tempInventoryArray.push(rooms[currentRoom]['inventory'].pop())
     //     console.log(tempInventoryArray)
@@ -193,15 +145,4 @@ function take(desiredItem) {
     //     rooms[currentRoom]['inventory'].push(tempInventoryArray.pop())
     //     console.log()
     // }
-}
-
-// let move = function () {
-//     return {
-//         room: function () {
-
-//         }
-
-
-
-//     }
 // }
